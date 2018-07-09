c2398cef5e9da540e0eb6f298f882a6c2300fe98
samuel gavidia <gavidiamamani@gmail.com>
2018-07-08 20:11:51 -0500

    config

--- angularjs_oracle/src/main/java/com/developer/angularjs_oracle/config/HibernateConfig.java
+++ angularjs_oracle/src/main/java/com/developer/angularjs_oracle/config/HibernateConfig.java
@@ -1,5 +1,64 @@
 package com.developer.angularjs_oracle.config;
 
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
 public class HibernateConfig {
 
+	// Change the below based on the DBMS you choose
+	private static final String DATABASE_URL="jdbc:oracle:@localhost:1521:xe";
+	private static final String DATABASE_DRIVER="oracle.jdbc.driver.OracleDriver";
+	private static final String DATABASE_DIALECT="org.hibernate.dialect.Oracle10gDialect";
+	private static final String DATABASE_USERNAME="user_details";
+	private static final String DATABASE_PASSWORD="12345";
+	
+	@Bean
+	public DataSource getDataSource(){
+		BasicDataSource dataSource=new BasicDataSource();
+		dataSource.setDriverClassName(DATABASE_DRIVER);
+		dataSource.setUrl(DATABASE_URL);
+		dataSource.setUsername(DATABASE_USERNAME);
+		dataSource.setPassword(DATABASE_PASSWORD);
+		return dataSource;
+		
+	}
+	
+	//sessionFactory bean will be available
+	@Bean
+	public SessionFactory getSessionFactory(DataSource dataSource){
+		LocalSessionFactoryBuilder builder=new LocalSessionFactoryBuilder(dataSource);
+		builder.addProperties(getHibernateProperties());
+		builder.scanPackages("com.developer.angularjs_oracle.dto");
+		return builder.buildSessionFactory();
+	}
+	
+	
+	// All the hibernate properties will be returned in this method
+	private Properties getHibernateProperties(){
+		Properties properties=new Properties();
+		properties.put("hibernate.dialect", DATABASE_DIALECT);
+		properties.put("hibernate.show_sql", "true");
+		properties.put("hibernate.format_sql", "true");
+		properties.put("hibernate.hbm2ddl.auto", "update");
+		return properties;
+	}
+	
+	// transactionManager bean
+	@Bean
+	public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory){
+		HibernateTransactionManager transactionManager=new HibernateTransactionManager(sessionFactory);
+		return transactionManager;
+	}
+	
 }
